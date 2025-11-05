d7ddbeea6b0bf2307157264e3724eb97e5e598a7
yalaka03 <surya.teja@students.iiit.ac.in>
2025-11-05 19:18:51 +0530

    cors

--- Reamdme.md
+++ Reamdme.md
@@ -1,2 +1,3 @@
 sudo apt-get update
 sudo apt-get install libgl1-mesa-glx
+https://attendance-system-backend-vkyx.onrender.com
--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
+    "https://yalaka03.github.io"
 ];
 
 module.exports = allowedOrigins;
